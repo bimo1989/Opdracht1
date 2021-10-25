15c6fe3f4d9302b0309e6aabe76eb925a7b1174a
Abdel Arfala <abdeljalil.arfala@gmail.com>
2021-10-25 20:11:21 +0200

    Initial commit

---
+++ RegisterController.java
@@ -0,0 +1,19 @@
+package Opdracht1;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class RegisterController {
+    @FXML
+    private TextField txtGebruikersnaam;
+
+    @FXML
+    private PasswordField txtWachtwoord;
+
+    @FXML
+    private void btnregistreer(ActionEvent actionEvent) {
+
+    }
+}
